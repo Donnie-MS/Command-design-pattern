82babb56524fedbd30ba6824f8dbf6fa73fa37a2
Orejas 2016 <joaomasgosandoval@gmail.com>
2025-11-04 20:55:04 -0300

    add Deposit Command

--- src/main/java/CommandDesignPattern/app/Deposit.java
+++ src/main/java/CommandDesignPattern/app/Deposit.java
@@ -1,5 +1,16 @@
 package CommandDesignPattern.app;
 
-public class Deposit {
+public class Deposit implements IOperacion{
+    private Account account;
+    private Double amount;
 
+    public Deposit(Account account, Double amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void execute() {
+        this.account.deposit(this.amount);
+    }
 }
---
+++ src/main/java/CommandDesignPattern/app/Invoker.java
@@ -0,0 +1,16 @@
+package CommandDesignPattern.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invoker {
+    private List<IOperacion> operaciones = new ArrayList<>();
+    
+    public void recibirOperacion(IOperacion operacion) {
+        this.operaciones.add(operacion);
+    }
+    
+    public void realizarOperaciones() {
+        this.operaciones.forEach(x -> x.execute());
+    }
+}
